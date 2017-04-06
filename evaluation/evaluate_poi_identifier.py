98bad53e771f525f81a90706395eac0e016a7723
Adrian Li <li.adrianmc@gmail.com>
2017-04-06 00:19:26 -0700

    finish the main lessons

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -27,5 +27,22 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+from sklearn.model_selection import train_test_split
 
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.3, random_state=42)
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+print zip(pred, labels_test)
+print accuracy_score(pred, labels_test)
+
+from sklearn.metrics import precision_score, recall_score
+print precision_score(labels_test, pred)
+print recall_score(labels_test, pred)
